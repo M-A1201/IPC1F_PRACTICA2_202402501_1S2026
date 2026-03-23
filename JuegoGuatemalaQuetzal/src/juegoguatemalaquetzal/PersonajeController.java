2012288231e3d2e35a504c485a3ebf77c86c44d1
M-A1201 <29467555@ingenieria.usac.edu.gt>
2026-03-23 22:09:08 +0100

    se implementa la clase controller la gestion y almacenamiento de peronajes mediante un arreglo

--- JuegoGuatemalaQuetzal/src/juegoguatemalaquetzal/PersonajeController.java
+++ JuegoGuatemalaQuetzal/src/juegoguatemalaquetzal/PersonajeController.java
@@ -6,37 +6,43 @@ package juegoguatemalaquetzal;
  * @author Manuel
  */
 public class PersonajeController {
- 
+ //arreglo que almacena todos los personajes del juego
     public static PersonajeModel[] personajes=new PersonajeModel[100];
     
+    //metodo para agregar personajes predefinidos del sistema
     public void agregarPersonajesQuemados(){
+        
+        //creacion de objetos tipo personaje
     PersonajeModel harry =new PersonajeModel();
     PersonajeModel draco= new PersonajeModel();
-    
+     
+    //creacion de objeto de tipo escoba
     EscobaModel nimbus2000=new EscobaModel();
     EscobaModel nimbus2001=new EscobaModel();
-    
+    //configuracion de la escoba nimbus 2000
     nimbus2000.setDormirSegundos(3);
     nimbus2000.setNombre("Nimbus 2000");
     nimbus2000.setIdEscoba(1);
     
+    //...
     nimbus2001.setDormirSegundos(2);
     nimbus2001.setNombre("Nimbus 2001");
     nimbus2001.setIdEscoba(2);
 
-    
+    //asignacion de datos al personaje harry
     harry.setIdpersonaje(1);
     harry.setCasa(Casa.GRIFINDOR);
     harry.setEscoba(nimbus2000);
     harry.setNombre("Harry Potter");
     
+    //...
     draco.setIdpersonaje(2);
     draco.setCasa(Casa.SLYTHERIN);
     draco.setEscoba(nimbus2001);
     draco.setNombre("Draco Malfoy");
     
     
-    
+    //se almacena los personajes en el arreglo 
     personajes[0]= harry;
     personajes[1]=draco;
     
