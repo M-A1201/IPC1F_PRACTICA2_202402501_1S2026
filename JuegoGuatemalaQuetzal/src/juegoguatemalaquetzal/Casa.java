320fd93199a4ac6ab55e9ad73e2734c289f52150
M-A1201 <29467555@ingenieria.usac.edu.gt>
2026-03-23 21:24:09 +0100

    se agrega enum casa con las posiciones de casas pra los personajes

--- JuegoGuatemalaQuetzal/src/juegoguatemalaquetzal/Casa.java
+++ JuegoGuatemalaQuetzal/src/juegoguatemalaquetzal/Casa.java
@@ -5,7 +5,9 @@ package juegoguatemalaquetzal;
  *
  * @author Manuel
  */
+//enum define la forma estructurada las casas disponibles para los personajes del juego
 public enum Casa {
+  //casas disponibles 
   GRIFINDOR,
   RAVENCLAW,
   SLYTHERIN,
