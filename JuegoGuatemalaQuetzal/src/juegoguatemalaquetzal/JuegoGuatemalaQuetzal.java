cff95e13c313bada3a46ceb0ca841675b72d93b7
M-A1201 <29467555@ingenieria.usac.edu.gt>
2026-03-23 21:49:46 +0100

    se implementa la clase principal del sistema para iniciar y mostrar la interfaz grafica

--- JuegoGuatemalaQuetzal/src/juegoguatemalaquetzal/JuegoGuatemalaQuetzal.java
+++ JuegoGuatemalaQuetzal/src/juegoguatemalaquetzal/JuegoGuatemalaQuetzal.java
@@ -5,13 +5,19 @@ package juegoguatemalaquetzal;
  *
  * @author Manuel
  */
+//clase principal para inicial la ejecucion del programa 
 public class JuegoGuatemalaQuetzal {
 
     public static void main(String[] args) {
+
+//se crea una instancia del controlador de peronaje 
         PersonajeController control=new PersonajeController();
+       //se agregan personajes 
         control.agregarPersonajesQuemados();
        
+        //se crea la vista principal del juego 
     VistaPrincipal vPrincipal=new VistaPrincipal();
+    //hacemos visible la ventana de la interfaz grafica 
     vPrincipal.setVisible(true);
     }
     
